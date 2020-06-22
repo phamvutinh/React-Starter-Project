4ea47e2cda84ee7bc9df5f660de58b70f56d3927
unknown <phamvu.tinh99@gmail.com>
2020-06-22 13:54:19 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const newVariable = "Hello";
+  const variable = "Hello";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Edited by Pham Tinh</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
